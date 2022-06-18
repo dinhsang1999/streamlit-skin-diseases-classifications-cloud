25b64eae3c33dab02971056cadb49d6e689b0225
dinhsang1999 <dr.dinh1999@gmail.com>
2022-06-19 01:04:29 +0700

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,26 +55,28 @@ if authentication_status:
     image_cover = Image.open('./image/cover.png')
     st.image(image_cover,use_column_width= True)
 
-    st.write("""
-    # Skin Diseases Detect Web App
+    with st.expander('About'):
+        st.write("""
+        # Skin Diseases Detect Web App
 
-    #### This app will detect `skin diseases`
+        #### This app will detect `skin diseases`
 
-    ***Skin cancer*** is by far the world's most common cancer. Among different skin cancer types, melanoma is particularly dangerous because of its ability to metastasize. Early detection is the key to success in skin cancer treatment. However, skin cancer diagnostic is still a challenge, even for experienced dermatologists, due to strong resemblances between benign and malignant lesions. To aid dermatologists in skin cancer diagnostic, we developed a deep learning system that can effectively and automatically classify skin lesions in the ISIC dataset. An end-to-end deep learning process, transfer learning technique, utilizing multiple pre-trained models, combining with class-weighted and focal loss function was applied for the classification process. The result was that our modified famous architectures with metadata could classify skin lesions in the ISIC dataset into one of the nine classes: (1) ***Actinic Keratosis***, (2) ***Basel Cell Carcinoma***, (3) ***Benign Keratosis***, (4) ***Dermatofibroma***, (5) ***Melanocytic nevi***, (6) ***Melanoma***, (7) ***Vascular Skin Lesion*** (8) ***Squamous Cell Carcinoma*** (9) ***Unknown*** with 93% accuracy and 97% and 99% for top 2 and top 3 accuracies, respectively. This deep learning system can potentially be integrated into computer-aided diagnosis systems that support dermatologists in skin cancer diagnostic.
+        ***Skin cancer*** is by far the world's most common cancer. Among different skin cancer types, melanoma is particularly dangerous because of its ability to metastasize. Early detection is the key to success in skin cancer treatment. However, skin cancer diagnostic is still a challenge, even for experienced dermatologists, due to strong resemblances between benign and malignant lesions. To aid dermatologists in skin cancer diagnostic, we developed a deep learning system that can effectively and automatically classify skin lesions in the ISIC dataset. An end-to-end deep learning process, transfer learning technique, utilizing multiple pre-trained models, combining with class-weighted and focal loss function was applied for the classification process. The result was that our modified famous architectures with metadata could classify skin lesions in the ISIC dataset into one of the nine classes: (1) ***Actinic Keratosis***, (2) ***Basel Cell Carcinoma***, (3) ***Benign Keratosis***, (4) ***Dermatofibroma***, (5) ***Melanocytic nevi***, (6) ***Melanoma***, (7) ***Vascular Skin Lesion*** (8) ***Squamous Cell Carcinoma*** (9) ***Unknown*** with 93% accuracy and 97% and 99% for top 2 and top 3 accuracies, respectively. This deep learning system can potentially be integrated into computer-aided diagnosis systems that support dermatologists in skin cancer diagnostic.
 
-    ***SAVE***: 
-    ```python
-    - Melanoma is malinant (dangerous)
-    - Others is benign (but also careful)
-    ```
+        ***SAVE***: 
+        ```python
+        - Melanoma is malinant (dangerous)
+        - Others is benign (but also careful)
+        ```
 
-    ##### Some examples:
-    """)
+        ##### Some examples:
+        """)
     # --- VISUAL DATA ---
-    if st.checkbox('Preview examples of 9 types diseases skin'):
+    with st.expander('Preview examples of 9 types diseases skin'):
         st.balloons()
         visual_diseases()
-    if st.checkbox('Preview dataset'):
+    with st.expander('Preview dataset'):
+        st.balloons()
         visual_dataset()
 
     # --- SIDEBAR: User input ---
